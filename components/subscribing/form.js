d9cfbd9773cccdb65448608247cd300352bb01e0
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-03-03 05:03:43 +0700

    fixing production bug

--- components/subscribing/form.js
+++ components/subscribing/form.js
@@ -1,14 +1,14 @@
 import Button from "../ui/button"
 
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message";
+// import { useForm } from "react-hook-form";
+// import { ErrorMessage } from "@hookform/error-message";
 
 export default function form() {
-    const {register, handleSubmit, formState:{ errors }, reset} = useForm({ criteriaMode: "all" })
+    // const {register, handleSubmit, formState:{ errors }, reset} = useForm({ criteriaMode: "all" })
 
-    const onSubmit = () => {
-      lg
-    }
+    // const onSubmit = () => {
+    //   lg
+    // }
 
   return (
     <div className='flex-center justify-between bg-white rounded-[32.5px] p-2'>
